b1a3ba41cba9130e297154b7029db6f21586f700
TeacherLoc <zZminhrauZz@gmail.com>
2023-09-25 19:46:12 +0700

    update

--- SachOnline/Controllers/SachOnlineController.cs
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SachOnline.Models;
 
 namespace SachOnline.Controllers
 {
     public class SachOnlineController : Controller
     {
         // GET: SachOnline
+
+
+
+
+        private dbSachOnlineDataContext data;
+
+       
+
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public SachOnlineController()
+        {
+            string connectionString = @"Data Source=(local);Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connectionString);
+        }
+        // GET: SachOnline
         public ActionResult Index()
         {
-            return View();
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
         }
+
         public ActionResult ChuDePartial()
         {
-            return PartialView();
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
         }
         public ActionResult NhaXuatBanPartial()
         {
